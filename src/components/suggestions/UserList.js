8d4f9e4826338337216365e44d4414ef7424483c
Vuong <vuong260902@gmail.com>
2021-09-30 10:29:16 +0700

    Update userList file

--- src/components/suggestions/UserList.js
+++ src/components/suggestions/UserList.js
@@ -4,13 +4,12 @@ import classes from "./UserList.module.css";
 import UserItem from "./UserItem";
 import { DUMMY_SUGGESTIONS } from "./DUMMY_SUGGESTIONs";
 
-const randomIndexs = [];
-while (randomIndexs.length < 5) {
-    const r = Math.floor(Math.random() * 20) + 1;
-    if (randomIndexs.indexOf(r) === -1) randomIndexs.push(r);
-}
-
 const UserList = () => {
+    const randomIndexs = [];
+    while (randomIndexs.length < 5) {
+        const r = Math.floor(Math.random() * 20) + 1;
+        if (randomIndexs.indexOf(r) === -1) randomIndexs.push(r);
+    }
     return (
         <React.Fragment>
             <div className={classes.header}>
